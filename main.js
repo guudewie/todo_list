3914f5781767481be2fce4787c4859023b39cf71
guudewie <philippthiessen12@gmail.com>
2023-08-23 10:24:36 +0200

    write factory functions for todo and projects

--- main.js
+++ main.js
@@ -7,26 +7,100 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://todo_list/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _todos_todos_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./todos/todos.js */ \"./src/todos/todos.js\");\n/* harmony import */ var _projects_projects_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./projects/projects.js */ \"./src/projects/projects.js\");\n\n\n\nconst newProject = (0,_projects_projects_js__WEBPACK_IMPORTED_MODULE_1__.Project)(\"School\", \"this project is for organizing my school work\");\n\nconsole.log(newProject)\n\n//# sourceURL=webpack://todo_list/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/projects/projects.js":
+/*!**********************************!*\
+  !*** ./src/projects/projects.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Project: () => (/* binding */ Project)\n/* harmony export */ });\n// Factory function for creating a Project\n\nconst Project = (name, description) => {\n\n    const getName = () => name;\n    const setName = (newName) => name = newName;\n\n    const getDescription = () => description;\n    const setDescription = (newDescription) => description = newDescription;\n\n    let _associatedToDos = [];\n\n    const getToDo = (index) => _associatedToDos[index];\n\n    const addToDo = (object) => _associatedToDos.push(object)\n    const removeToDo = (index) => _associatedToDos.splice(index, 1)\n\n\n    return {getName,\n            setName,\n            setDescription,\n            getDescription,\n            getToDo,\n            addToDo,\n            removeToDo,\n        }\n}\n\n\n\n//# sourceURL=webpack://todo_list/./src/projects/projects.js?");
+
+/***/ }),
+
+/***/ "./src/todos/todos.js":
+/*!****************************!*\
+  !*** ./src/todos/todos.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   ToDo: () => (/* binding */ ToDo)\n/* harmony export */ });\n// Factory function for creating a ToDo\n\nconst ToDo = (name, description, dueDate, status) => {\n\n    const getName = () => name;\n    const setName = (newName) => name = newName;\n\n    const getDescription = () => description;\n    const setDescription = (newDescription) => description = newDescription;\n\n    const getDueDate = () => dueDate;\n    const setDueDate = (newDueDate) => dueDate = newDueDate; \n\n    const getStatus = () => status\n    const toggleStatus = () => status = status ? false : true;\n\n\n    return {getName,\n            setName, \n            setDescription,\n            getDescription,\n            getDueDate,\n            setDueDate,\n            toogleStatus,\n            toggleStatus}\n}\n\n\n\n//# sourceURL=webpack://todo_list/./src/todos/todos.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
